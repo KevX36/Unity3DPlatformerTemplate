94bab880b9b6c5173114a2a41217d5fbedcaeeed
KevX36 <kevincard2007@gmail.com>
2026-01-30 10:30:24 -0400

    made it actually look for escape

--- Assets/Exitbutton.cs
+++ Assets/Exitbutton.cs
@@ -9,7 +9,7 @@ public class Exitbutton : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Equals))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             Application.Quit();
 
